593769546764e8d37d255b2f3d32729a73f524f5
TusharJarif10 <meheditushar76@gmail.com>
2021-06-09 11:48:50 +0600

    update.

---
+++ camera_vision/lib/main.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+
+import 'login_page/login_page.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: LoginPage(title: 'Login'),
+    );
+  }
+}
